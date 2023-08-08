85d8f96ae745420fa4b4d9e3b5e0c2fb8e30c4ef
Maxython <mixython@gmail.com>
2023-08-08 20:55:53 +0300

    script/utils/set_data.sh: fix gcc-arm [skip ci]

--- script/utils/set_data.sh
+++ script/utils/set_data.sh
@@ -6,3 +6,8 @@ if [ ! -d /data ]; then
 	chown ${GPKG_DEV_USER_NAME} /data
 	chgrp ${GPKG_DEV_USER_NAME} /data
 fi
+
+if [ $ARCH = "arm" ] && [ ! -f $GLIBC_PREFIX/lib/libgcc_s.so.1 ]; then
+	sudo -H -u ${GPKG_DEV_USER_NAME} mkdir -p $GLIBC_PREFIX/lib
+	sudo -H -u ${GPKG_DEV_USER_NAME} ln -s /usr/arm-linux-gnueabihf/lib/libgcc_s.so.1 $GLIBC_PREFIX/lib/libgcc_s.so.1
+fi
