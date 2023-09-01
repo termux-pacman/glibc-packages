9dbc06e84d785dcd0ff0896d6dca8f2480416986
Maxython <mixython@gmail.com>
2023-09-01 16:24:51 +0300

    gpkg/libunistring: new package

---
+++ gpkg/libunistring/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libunistring/
+TERMUX_PKG_DESCRIPTION="Library for manipulating Unicode strings and C strings"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256="827c1eb9cb6e7c738b171745dac0888aa58c5924df2e59239318383de0729b98"
+TERMUX_PKG_DEPENDS="glibc"
