479235342c5b0e3bce4a486f0f118e19829a041f
Maxython <mixython@gmail.com>
2023-09-06 22:39:48 +0300

    rebuild some packages in gpkg

--- gpkg/expat/build.sh
+++ gpkg/expat/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=2.5.0
 TERMUX_PKG_SRCURL=https://github.com/libexpat/libexpat/releases/download/R_${TERMUX_PKG_VERSION//./_}/expat-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=6f0e6e01f7b30025fa05c85fdad1e5d0ec7fd35d9f61b22f34998de11969ff67
-TERMUX_PKG_DEPENDS="glibc"
+TERMUX_PKG_DEPENDS="glibc, gcc-glibc-libs-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-docbook"
