bd339e8476fbccff0b27df2d921a2746857ab3dc
Ivan Max <mixython@gmail.com>
2023-09-06 20:59:28 +0000

    gpkg/libisl: new package (#90)

---
+++ gpkg/libisl/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://libisl.sourceforge.io/
+TERMUX_PKG_DESCRIPTION="Library for manipulating sets and relations of integer points bounded by linear constraints"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=0.26
+TERMUX_PKG_SRCURL=https://libisl.sourceforge.io/isl-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=a0b5cb06d24f9fa9e77b55fabbe9a3c94a336190345c2555f9915bb38e976504
+TERMUX_PKG_DEPENDS="libgmp-glibc"
