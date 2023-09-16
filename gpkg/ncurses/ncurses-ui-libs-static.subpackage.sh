aa9f7d62be3c4b7ff839d59c1892c6917d237f3b
Ivan Max <mixython@gmail.com>
2023-09-16 10:36:33 +0000

    gpkg/binutils-libs: new pkg (#100)
    fix dependencies in gpkg/ncurses

--- gpkg/ncurses/ncurses-ui-libs-static.subpackage.sh
+++ gpkg/ncurses/ncurses-ui-libs-static.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="glibc/lib/libform*.a glibc/lib/libmenu*.a glibc/lib/libpanel*.a"
 TERMUX_SUBPKG_DESCRIPTION="Static libraries for terminal user interfaces based on ncurses"
-TERMUX_SUBPKG_DEPENDS="ncurses-ui-libs"
+TERMUX_SUBPKG_DEPENDS="ncurses-ui-libs-glibc"
--- gpkg/ncurses/ncurses-ui-libs.subpackage.sh
+++ gpkg/ncurses/ncurses-ui-libs.subpackage.sh
@@ -7,5 +7,3 @@ glibc/lib/pkgconfig/menu*.pc
 glibc/lib/pkgconfig/panel*.pc
 "
 TERMUX_SUBPKG_DESCRIPTION="Libraries for terminal user interfaces based on ncurses"
-TERMUX_SUBPKG_BREAKS="ncurses (<< 6.4)"
-TERMUX_SUBPKG_REPLACES="ncurses (<< 6.4)"
