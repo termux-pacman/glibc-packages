f725411d14f48693fa741fce97dbe3122db50bec
Maxython <mixython@gmail.com>
2023-09-18 12:08:54 +0300

    new pkgs to gpkg gpkg/libnsl gpkg/libtirpc gpkg/libuv

---
+++ gpkg/libuv/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://libuv.org
+TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
+TERMUX_PKG_LICENSE="MIT, BSD 2-Clause, ISC, BSD 3-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=1.46.0
+TERMUX_PKG_SRCURL=https://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=111f83958b9fdc65f1489195d25f342b9f7a3e683140c60e62c00fbaccddddce
+TERMUX_PKG_DEPENDS="libnsl-glibc"
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
