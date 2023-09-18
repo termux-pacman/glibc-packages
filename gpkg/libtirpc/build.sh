d1f2691e8b829afca1be172fcf503384a7fbd13b
Ivan Max <mixython@gmail.com>
2023-09-18 10:28:57 +0000

    new pkgs to gpkg (#104)
    gpkg/libnsl
    gpkg/libtirpc
    gpkg/libuv

---
+++ gpkg/libtirpc/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="http://git.linux-nfs.org/?p=steved/libtirpc.git"
+TERMUX_PKG_DESCRIPTION="Transport Independent RPC library"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/libtirpc/libtirpc-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=6474e98851d9f6f33871957ddee9714fdcd9d8a5ee9abb5a98d63ea2e60e12f3
+TERMUX_PKG_DEPENDS="krb5-glibc"
