ee54c23b0682b0014c7160ab524f721038005b0e
Maxython <mixython@gmail.com>
2023-10-05 11:40:26 +0300

    gpkg/attr: enable gettext

--- gpkg/attr/build.sh
+++ gpkg/attr/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Utilities for manipulating filesystem extended attribute
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=2.5.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/attr/attr-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bae1c6949b258a0d68001367ce0c741cebdacdd3b62965d17e5eb23cd78adaf8
 TERMUX_PKG_DEPENDS="glibc, gcc-libs-glibc"
+TERMUX_PKG_BUILD_DEPENDS="gettext-glibc"
 TERMUX_PKG_BUILD_IN_SRC=true
-# At the moment we don't have gettext-glibc
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gettext=no"
