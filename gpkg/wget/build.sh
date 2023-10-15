da2fc2162ab90a99eb2c24f20f2d164aaf469975
Ivan Max <mixython@gmail.com>
2023-10-15 18:01:26 +0000

    gpkg/wget: new pkg (#125)

---
+++ gpkg/wget/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
+TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=1.21.4
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
+TERMUX_PKG_SHA256=3683619a5f50edcbccb1720a79006fa37bf9b9a255a8c5b48048bc3c7a874bd9
+TERMUX_PKG_DEPENDS="libidn2-glibc, libuuid-glibc, openssl-glibc, pcre2-glibc, zlib-glibc, libnettle-glibc, libpsl-glibc, libgnutls-glibc"
+TERMUX_PKG_CONFFILES="glibc/etc/wgetrc"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-rpath
+--enable-nls
+--with-ssl=gnutls
+"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lbrotlicommon"
+}
