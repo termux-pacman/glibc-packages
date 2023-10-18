98c87f776125ba949333d98281c5cef8e725bf64
Maxython <mixython@gmail.com>
2023-10-18 12:25:08 +0300

    new pkgs to gpkg gpkg/vulkan-icd-loader gpkg/giflib gpkg/gifsicle gpkg/libjpeg-turbo gpkg/xmlto gpkg/libunwind

---
+++ gpkg/giflib/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/giflib/
+TERMUX_PKG_DESCRIPTION="A library for reading and writing gif images"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=5.2.1
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/giflib-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd
+TERMUX_PKG_DEPENDS="glibc"
+TERMUX_PKG_BUILD_DEPENDS="xmlto-glibc"
+TERMUX_PKG_BUILD_IN_SRC=true
---
+++ gpkg/libjpeg-turbo/libjpeg-turbo-progs.subpackage.sh
@@ -0,0 +1,2 @@
+TERMUX_SUBPKG_INCLUDE="glibc/bin/ glibc/share/man/man1/"
+TERMUX_SUBPKG_DESCRIPTION="Programs for manipulating JPEG files"
