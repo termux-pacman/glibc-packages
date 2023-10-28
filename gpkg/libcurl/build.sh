524b943594f262c503f9b7a04937ad30008154e1
Maxython <mixython@gmail.com>
2023-10-28 17:43:13 +0300

    upd pkgs to gpkg: gpkg/krb5 gpkg/libcurl gpkg/libdrm gpkg/libnghttp2 gpkg/libnsl gpkg/oniguruma gpkg/openssl gpkg/p11-kit gpkg/vulkan-headers gpkg/libunwind gpkg/vulkan-icd-loader

--- gpkg/libcurl/build.sh
+++ gpkg/libcurl/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://curl.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="8.3.0"
+TERMUX_PKG_VERSION=8.4.0
 TERMUX_PKG_SRCURL=https://github.com/curl/curl/releases/download/curl-${TERMUX_PKG_VERSION//./_}/curl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=376d627767d6c4f05105ab6d497b0d9aba7111770dd9d995225478209c37ea63
+TERMUX_PKG_SHA256=16c62a9c4af0f703d28bda6d7bbf37ba47055ad3414d70dec63e2e6336f2a82d
 TERMUX_PKG_DEPENDS="libnghttp2-glibc, libssh2-glibc, openssl-glibc (>= 3.0.3), krb5-glibc, brotli-glibc"
 TERMUX_PKG_ESSENTIAL=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="LDFLAGS='-lbrotlidec'"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-ca-bundle=$TERMUX_PREFIX/etc/ssl/certs/ca-certificates.crt
 --with-ca-path=$TERMUX_PREFIX/etc/ssl/certs
