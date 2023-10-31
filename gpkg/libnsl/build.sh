8de0be31976e511f6616281df0939dbc0e6c4781
Ivan Max <mixython@gmail.com>
2023-10-31 13:20:04 +0000

    upd pkgs to gpkg: (#145)
    gpkg/krb5
    gpkg/libcurl
    gpkg/libdrm
    gpkg/libnghttp2
    gpkg/libnsl
    gpkg/oniguruma
    gpkg/openssl
    gpkg/p11-kit
    gpkg/vulkan-headers
    gpkg/libunwind
    gpkg/vulkan-icd-loader

--- gpkg/libnsl/build.sh
+++ gpkg/libnsl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/thkukuk/libnsl
 TERMUX_PKG_DESCRIPTION="Public client interface library for NIS(YP)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_VERSION=2.0.1
 TERMUX_PKG_SRCURL=https://github.com/thkukuk/libnsl/archive/v${TERMUX_PKG_VERSION}/libnsl-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=eb37be57c1cf650b3a8a4fc7cd66c8b3dfc06215b41956a16325a9388171bc40
+TERMUX_PKG_SHA256=59048b53be8d3904bf939313debf13956a881b0de79da40f7719a77bcd1e9c53
 TERMUX_PKG_DEPENDS="libtirpc-glibc"
 
 termux_step_pre_configure() {
