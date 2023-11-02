56cabdfc9729632202ce415429e8d89ffa740fb0
Ivan Max <mixython@gmail.com>
2023-11-02 16:29:38 +0000

    adding pkgs and fixing pkgs (#151)

--- gpkg/wget/build.sh
+++ gpkg/wget/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=1.21.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=3683619a5f50edcbccb1720a79006fa37bf9b9a255a8c5b48048bc3c7a874bd9
 TERMUX_PKG_DEPENDS="libidn2-glibc, libuuid-glibc, openssl-glibc, pcre2-glibc, zlib-glibc, libnettle-glibc, libpsl-glibc, libgnutls-glibc"
@@ -10,7 +11,7 @@ TERMUX_PKG_CONFFILES="glibc/etc/wgetrc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-rpath
 --enable-nls
---with-ssl=gnutls
+--with-ssl=openssl
 "
 
 termux_step_pre_configure() {
