cc5cefb2035cee0a29247ea856cbed36812da649
Maxython <mixython@gmail.com>
2023-11-06 21:24:17 +0300

    gpkg/{libxml2,libxslt}: enabling compilation with python

--- gpkg/libxml2/build.sh
+++ gpkg/libxml2/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 _MAJOR_VERSION=2.11
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libxml2/${_MAJOR_VERSION}/libxml2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3727b078c360ec69fa869de14bd6f75d7ee8d36987b071e6928d4720a28df3a6
-# At the moment we don't have python-glibc
-#TERMUX_PKG_SETUP_PYTHON=true
+TERMUX_PKG_SETUP_PYTHON=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---without-python
+--with-python
 --with-history
 --with-icu
 --with-threads
---
+++ gpkg/libxml2/libxml2-python-static.subpackage.sh
@@ -0,0 +1,6 @@
+TERMUX_SUBPKG_INCLUDE="
+glibc/lib/python*/**/*.a
+glibc/lib/python*/**/*.la
+"
+TERMUX_SUBPKG_DESCRIPTION="Static libraries for libxml2-python"
+TERMUX_SUBPKG_DEPENDS="libxml2-python-glibc"
