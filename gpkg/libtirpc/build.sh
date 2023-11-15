b711597861b6fd89ad3d3d3b873198a04549d138
Ivan Max <mixython@gmail.com>
2023-11-15 19:39:50 +0000

    upd pkgs to gpkg: (#159)
    gpkg/libtirpc
    gpkg/p11-kit
    gpkg/vulkan-headers
    gpkg/vulkan-icd-loader
    
    gpkg/glibc: chown fix for i686

--- gpkg/libtirpc/build.sh
+++ gpkg/libtirpc/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE="http://git.linux-nfs.org/?p=steved/libtirpc.git"
 TERMUX_PKG_DESCRIPTION="Transport Independent RPC library"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.3.4
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/libtirpc/libtirpc-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6474e98851d9f6f33871957ddee9714fdcd9d8a5ee9abb5a98d63ea2e60e12f3
+TERMUX_PKG_SHA256=1e0b0c7231c5fa122e06c0609a76723664d068b0dba3b8219b63e6340b347860
 TERMUX_PKG_DEPENDS="krb5-glibc"
