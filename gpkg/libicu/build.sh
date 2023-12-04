7380c7787cdd9218293736197baf419485374b80
Maxython <mixython@gmail.com>
2023-12-04 11:56:16 +0300

    gpkg/libicu: fix for arm

--- gpkg/libicu/build.sh
+++ gpkg/libicu/build.sh
@@ -5,15 +5,13 @@ TERMUX_PKG_LICENSE="BSD"
 # this hack to be able to find the license file.
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-# Never forget to always bump revision of reverse dependencies and rebuild them
-# when bumping "major" version.
 TERMUX_PKG_VERSION=73.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
 TERMUX_PKG_SHA256=818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1
 TERMUX_PKG_DEPENDS="gcc-libs-glibc, bash-glibc"
 TERMUX_PKG_HOSTBUILD=true
-TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="--disable-samples --disable-tests"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-samples --disable-tests --with-cross-build=$TERMUX_PKG_HOSTBUILD_DIR"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-cross-build=$TERMUX_PKG_HOSTBUILD_DIR"
 
 termux_step_post_get_source() {
 	TERMUX_PKG_SRCDIR+="/source"
