9b2f89e02aa489edfe7bce485db9e9a3260d63d2
Maxython <mixython@gmail.com>
2024-01-05 23:36:28 +0300

    global update pkgs gpkg/box64 gpkg/box86 gpkg/libicu gpkg/liblzma gpkg/libsqlite gpkg/libuv gpkg/libxml2 gpkg/libxslt gpkg/nlohmann-json gpkg/p11-kit gpkg/python-pip gpkg/tcl gpkg/util-linux gpkg/vulkan-headers gpkg/cmake gpkg/libcurl gpkg/libgcrypt

--- gpkg/libicu/build.sh
+++ gpkg/libicu/build.sh
@@ -5,16 +5,18 @@ TERMUX_PKG_LICENSE="BSD"
 # this hack to be able to find the license file.
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=73.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=74.2
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
-TERMUX_PKG_SHA256=818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1
+TERMUX_PKG_SHA256=5e4fb11d6a3e6b85afb55de8da8a71538f1d8fd64fce893986b37d60e5bb0091
 TERMUX_PKG_DEPENDS="gcc-libs-glibc, bash-glibc"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-cross-build=$TERMUX_PKG_HOSTBUILD_DIR"
 
 termux_step_post_get_source() {
+	rm ${TERMUX_PKG_SRCDIR}/LICENSE
+	cp ${TERMUX_PKG_BUILDER_DIR}/LICENSE ${TERMUX_PKG_SRCDIR}
 	TERMUX_PKG_SRCDIR+="/source"
+	sed -r -i 's/(for ac_prog in )clang(\+\+)? /\1/g' ${TERMUX_PKG_SRCDIR}/configure
 	find . -type f | xargs touch
 }
 
