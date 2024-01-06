cc53da919dbde6033841e746a13a765676e0d9c3
Ivan Max <mixython@gmail.com>
2024-01-06 16:33:57 +0000

    global update pkgs (#178)
    gpkg/box64
    gpkg/box86
    gpkg/libicu
    gpkg/liblzma
    gpkg/libsqlite
    gpkg/libuv
    gpkg/libxml2
    gpkg/libxslt
    gpkg/nlohmann-json
    gpkg/p11-kit
    gpkg/python-pip
    gpkg/tcl
    gpkg/util-linux
    gpkg/vulkan-headers
    gpkg/cmake
    gpkg/libcurl
    gpkg/libgcrypt

--- gpkg/libgcrypt/build.sh
+++ gpkg/libgcrypt/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="General purpose cryptographic library based on the code 
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1, BSD 3-Clause, MIT, Public Domain"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.LIB, LICENSES"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.10.2
+TERMUX_PKG_VERSION=1.10.3
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=3b9c02a004b68c256add99701de00b383accccf37177e0d6c58289664cce0c03
+TERMUX_PKG_SHA256=8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa
 TERMUX_PKG_DEPENDS="libgpg-error-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-padlock-support
