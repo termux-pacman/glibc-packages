a7e7e901878f82b0072b62b45a879fec2187252b
Maxython <mixython@gmail.com>
2024-01-06 20:36:22 +0300

    global update pkgs gpkg/doxygen gpkg/fontconfig gpkg/gettext gpkg/libcap-ng gpkg/libdrm gpkg/libelf gpkg/libgnutls gpkg/vulkan-headers

--- gpkg/libcap-ng/build.sh
+++ gpkg/libcap-ng/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://people.redhat.com/sgrubb/libcap-ng/
 TERMUX_PKG_DESCRIPTION="Library making programming with POSIX capabilities easier than traditional libcap"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="0.8.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.8.4"
 TERMUX_PKG_SRCURL=https://github.com/stevegrubb/libcap-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e542e9139961f0915ab5878427890cdc7762949fbe216bd0cb4ceedb309bb854
+TERMUX_PKG_SHA256=5615c76a61039e283a6bd107c4faf345ae5ad4dcd45907defe5e474d8fdb6fd2
 TERMUX_PKG_DEPENDS="glibc, gcc-libs-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-python
