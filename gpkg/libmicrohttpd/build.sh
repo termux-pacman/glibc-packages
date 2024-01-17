8219c7e470c61a49d24a0a5d528e42990d1ad55d
Maxython <mixython@gmail.com>
2024-01-17 23:12:07 +0300

    rebuild some pkgs gpkg/libmicrohttpd gpkg/giflib

--- gpkg/libmicrohttpd/build.sh
+++ gpkg/libmicrohttpd/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to ru
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=0.9.77
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9e7023a151120060d2806a6ea4c13ca9933ece4eacfc5c9464d20edddb76b0a0
 TERMUX_PKG_DEPENDS="libgnutls-glibc"
@@ -15,3 +16,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-messages
 --with-pic
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lbrotlicommon"
+}
