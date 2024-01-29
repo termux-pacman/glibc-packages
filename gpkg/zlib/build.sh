91015eacef970e905b48c2c3024a034547e54b32
Maxython <mixython@gmail.com>
2024-01-29 12:09:24 +0300

    update pkgs gpkg/fakeroot gpkg/glib gpkg/libjpeg-turbo gpkg/liblzma gpkg/libnghttp2 gpkg/vulkan-headers gpkg/zlib gpkg/vulkan-icd-loader

--- gpkg/zlib/build.sh
+++ gpkg/zlib/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.zlib.net/
 TERMUX_PKG_DESCRIPTION="Compression library implementing the deflate compression method found in gzip and PKZIP"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_VERSION=1.3.1
 TERMUX_PKG_SRCURL=https://www.zlib.net/zlib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8a9ba2898e1d0d774eca6ba5b4627a11e5588ba85c8851336eb38de4683050a7
+TERMUX_PKG_SHA256=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
 TERMUX_PKG_DEPENDS="glibc"
 
 termux_step_configure() {
