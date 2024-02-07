13d8e4eb83cf9bd4ed3e61c73c2a515d8012726f
Ivan Max <mixython@gmail.com>
2024-02-07 07:15:19 +0000

    update pkgs (#197)
    gpkg/ca-certificates
    gpkg/cmake
    gpkg/libcurl
    gpkg/libpng
    gpkg/libxml2
    gpkg/openssl
    gpkg/python-pip
    gpkg/vulkan-headers
    gpkg/vulkan-tools
    gpkg/xkeyboard-config
    gpkg/libexpat
    gpkg/libgc

--- gpkg/libpng/build.sh
+++ gpkg/libpng/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.libpng.org/pub/png/libpng.html
 TERMUX_PKG_DESCRIPTION="Official PNG reference library"
 TERMUX_PKG_LICENSE="Libpng"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.6.40
+TERMUX_PKG_VERSION=1.6.42
 TERMUX_PKG_SRCURL=https://download.sourceforge.net/libpng/libpng-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1
+TERMUX_PKG_SHA256=c919dbc11f4c03b05aba3f8884d8eb7adfe3572ad228af972bb60057bdb48450
 TERMUX_PKG_DEPENDS="zlib-glibc, bash-glibc"
 
 termux_step_post_make_install() {
