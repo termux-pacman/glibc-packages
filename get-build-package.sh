e40b9626df56b7345c939c03ac489a33b95ec53f
Ivan Max <mixython@gmail.com>
2024-02-15 18:03:04 +0000

    update pkgs (#199)
    gpkg/jq
    gpkg/libaom
    gpkg/libgnutls
    gpkg/libsqlite
    gpkg/libuv
    gpkg/perl
    gpkg/python
    gpkg/binutils-libs
    gpkg/libdav1d

--- get-build-package.sh
+++ get-build-package.sh
@@ -2,7 +2,9 @@
 
 # Script that installs build-package.sh to compile glibc packages
 
-git clone https://github.com/termux/termux-packages.git
+BRANCH="master"
+
+git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
 
 for i in build-package.sh clean.sh packages x11-packages scripts; do
 	cp -r ./termux-packages/${i} ./
