c8ef97ebf1ca74150a55e8b2538d4f16790f182a
Ivan Max <mixython@gmail.com>
2024-02-18 12:58:35 +0000

    update and improvement pkgs (#201)
    gpkg/mesa
    gpkg/pcre2
    +rebuild gpkg/spirv-llvm-translator
    +new pkg: gpkg/mtdev
    +fix gpkg/glibc: disable flag TCSAFLUSH

--- gpkg/pcre2/build.sh
+++ gpkg/pcre2/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.pcre.org
 TERMUX_PKG_DESCRIPTION="Perl 5 compatible regular expression library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="10.42"
+TERMUX_PKG_VERSION="10.43"
 TERMUX_PKG_SRCURL=https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${TERMUX_PKG_VERSION}/pcre2-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8d36cd8cb6ea2a4c2bb358ff6411b0c788633a2a45dabbf1aeb4b701d1b5e840
+TERMUX_PKG_SHA256=e2a53984ff0b07dfdb5ae4486bbb9b21cca8e7df2434096cc9bf1b728c350bcb
 TERMUX_PKG_DEPENDS="readline-glibc, zlib-glibc, bash-glibc, libbz2-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-pcre2-16
