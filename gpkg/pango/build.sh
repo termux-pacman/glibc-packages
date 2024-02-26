0a1965ed4ecfbffec3d6ba5ef1437a75a1db9586
Ivan Max <mixython@gmail.com>
2024-02-26 19:35:15 +0000

    upd pkgs to gpkg (#206)
    gpkg/bash-completion
    gpkg/gettext
    gpkg/liblzma
    gpkg/libpng
    gpkg/linux-api-headers
    gpkg/pango
    gpkg/spirv-headers
    gpkg/spirv-tools
    
    fix gpkg/glibc

--- gpkg/pango/build.sh
+++ gpkg/pango/build.sh
@@ -2,8 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.51.1
+TERMUX_PKG_VERSION=1.51.2
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/pango/-/archive/${TERMUX_PKG_VERSION}/pango-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ea92cd570cdba62ca52cc0a7c9ea3cd311b6da3f0328a5aa8a4a81b0a74944a5
+TERMUX_PKG_SHA256=82b4c5232e448865865e595008b6aef2481032c898d8bbfc60fd36b680585d75
 TERMUX_PKG_DEPENDS="libcairo-glibc, fribidi-glibc, harfbuzz-glibc, libthai-glibc, libxft-glibc"
 TERMUX_PKG_BUILD_DEPENDS="gobject-introspection-glibc"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lm"
+}
