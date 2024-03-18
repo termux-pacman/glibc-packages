2b89074c3bb2f0356335267236e7fbb78798187f
Ivan Max <mixython@gmail.com>
2024-03-18 08:57:14 +0000

    update pkgs (#214)
    gpkg/attr
    gpkg/ca-certificates
    gpkg/gifsicle
    gpkg/libdav1d
    gpkg/libffi
    gpkg/libgpg-error
    gpkg/libmicrohttpd
    gpkg/libxcursor
    gpkg/libxdmcp
    gpkg/libxext
    gpkg/libxkbfile
    gpkg/libxml2
    gpkg/mesa
    gpkg/tcl
    gpkg/xorg-xkbcomp
    gpkg/xorg-xmessage

--- gpkg/libgpg-error/build.sh
+++ gpkg/libgpg-error/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/libgpg-error/
 TERMUX_PKG_DESCRIPTION="Small library that defines common error values for all GnuPG components"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.47
+TERMUX_PKG_VERSION=1.48
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb
+TERMUX_PKG_SHA256=89ce1ae893e122924b858de84dc4f67aae29ffa610ebf668d5aa539045663d6f
 TERMUX_PKG_DEPENDS="glibc, bash-glibc"
 
 termux_step_pre_configure() {
