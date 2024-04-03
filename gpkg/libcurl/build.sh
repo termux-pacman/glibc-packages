ca5e015655bcaa818e2fc5e8c301f32e7be8f292
Maxython <mixython@gmail.com>
2024-04-03 11:32:46 +0300

    update pkgs to gpkg gpkg/cmake gpkg/glib gpkg/libcurl gpkg/libx11 gpkg/strace gpkg/vulkan-headers gpkg/zstd

--- gpkg/libcurl/build.sh
+++ gpkg/libcurl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://curl.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=8.6.0
+TERMUX_PKG_VERSION=8.7.1
 TERMUX_PKG_SRCURL=https://github.com/curl/curl/releases/download/curl-${TERMUX_PKG_VERSION//./_}/curl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3ccd55d91af9516539df80625f818c734dc6f2ecf9bada33c76765e99121db15
+TERMUX_PKG_SHA256=6fea2aac6a4610fbd0400afb0bcddbe7258a64c63f1f68e5855ebc0c659710cd
 TERMUX_PKG_DEPENDS="libnghttp2-glibc, libssh2-glibc, openssl-glibc (>= 3.0.3), krb5-glibc, brotli-glibc, libpsl-glibc"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="LDFLAGS='-lbrotlidec'"
