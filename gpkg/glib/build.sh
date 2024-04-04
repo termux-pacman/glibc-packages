c5357f8ebc65ceb42eed303007114111959fe77e
Ivan Max <mixython@gmail.com>
2024-04-04 06:36:29 +0000

    update pkgs to gpkg (#220)
    gpkg/cmake
    gpkg/glib
    gpkg/libcurl
    gpkg/libx11
    gpkg/strace
    gpkg/vulkan-headers
    gpkg/zstd

--- gpkg/glib/build.sh
+++ gpkg/glib/build.sh
@@ -2,19 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="2.78.4"
+TERMUX_PKG_VERSION="2.80.0"
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION%.*}/glib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=24b8e0672dca120cc32d394bccb85844e732e04fe75d18bb0573b2dbc7548f63
+TERMUX_PKG_SHA256=8228a92f92a412160b139ae68b6345bd28f24434a7b5af150ebe21ff587a561d
 TERMUX_PKG_DEPENDS="libffi-glibc, pcre2-glibc, util-linux-glibc, zlib-glibc, openssl-glibc, libunwind-glibc"
-TERMUX_PKG_PYTHON_COMMON_DEPS="pygments, itstool"
-# -Dgtk_doc=true
-# -Dsysprof=enabled
+TERMUX_PKG_PYTHON_COMMON_DEPS="pygments, itstool, packaging"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --default-library both
 -Druntime_dir=$TERMUX_PREFIX/var/run
--Dglib_debug=disabled
--Dselinux=disabled
--Dman=true
+-D glib_debug=disabled
+-D documentation=true
+-D introspection=disabled
+-D man-pages=enabled
+-D selinux=disabled
 "
 
 termux_step_pre_configure() {
