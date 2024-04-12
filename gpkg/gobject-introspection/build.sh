45e9ea219ece2c5feb7f6ce9d8fba54287e0d1a5
Maxython <mixython@gmail.com>
2024-04-12 23:24:37 +0300

    duplicate file fixes from `python-glibc`

--- gpkg/gobject-introspection/build.sh
+++ gpkg/gobject-introspection/build.sh
@@ -3,18 +3,19 @@ TERMUX_PKG_DESCRIPTION="Uniform machine readable API"
 TERMUX_PKG_LICENSE="LGPL-2.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION="1.80.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gobject-introspection/${TERMUX_PKG_VERSION%.*}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a1df7c424e15bda1ab639c00e9051b9adf5cea1a9e512f8a603b53cd199bc6d8
 TERMUX_PKG_DEPENDS="libgirepository-glibc, python-glibc"
 TERMUX_PKG_BUILD_DEPENDS="coreutils-glibc"
 TERMUX_PKG_MESON_NATIVE=true
-TERMUX_PKG_RM_AFTER_INSTALL="glibc/lib/python*"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dpython=$TERMUX_PREFIX/bin/python3
 "
 
 termux_step_pre_configure() {
 	CPPFLAGS+="-I$TERMUX_PREFIX/include/python${TERMUX_PYTHON_VERSION} -I$TERMUX_PREFIX/include/python${TERMUX_PYTHON_VERSION}/cpython"
+	LDFLAGS+=" -lmount"
 }
 
 termux_step_post_make_install() {
--- gpkg/libxcb/build.sh
+++ gpkg/libxcb/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION="1.16.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/lib/libxcb-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=f24d187154c8e027b358fc7cb6588e35e33e6a92f11c668fe77396a7ae66e311
 TERMUX_PKG_DEPENDS="libxau-glibc, libxdmcp-glibc, xcb-proto-glibc"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros-glibc, libxslt-glibc, xorgproto-glibc"
-TERMUX_PKG_RM_AFTER_INSTALL="glibc/lib/python*"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-xinput
 --enable-xkb
