7e143f8a149c62ba6371ba8b7e54121534bebbc8
Maxython <mixython@gmail.com>
2024-04-16 13:06:40 +0300

    update pkgs gpkg/cmake gpkg/libxcb gpkg/mesa gpkg/pango gpkg/vulkan-headers gpkg/xcb-proto gpkg/vulkan-headers gpkg/vulkan-tools gpkg/fribidi gpkg/xorg-util-macros gpkg/xorgproto gpkg/libarchive gpkg/libclc gpkg/coreutils

--- gpkg/libclc/build.sh
+++ gpkg/libclc/build.sh
@@ -2,19 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://libclc.llvm.org/
 TERMUX_PKG_DESCRIPTION="Library requirements of the OpenCL C programming language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=18.1.3
+TERMUX_PKG_VERSION=18.1.4
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/libclc-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=b117efb9a27ab923e03e565435f30ca8c5c1624d0832a09e32d14c3eb4995a7c
-TERMUX_PKG_BUILD_DEPENDS="clang-glibc, lld-glibc, python-glibc, spirv-llvm-translator-glibc"
+TERMUX_PKG_SHA256=81e687138a35eeaee70f0fdce4a7a892b94e3c0f310e3f0efa640efd0af2c29f
+TERMUX_PKG_BUILD_DEPENDS="clang-glibc, python-glibc, spirv-llvm-translator-glibc"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_configure() {
-	termux_setup_cmake
 	termux_setup_ninja
 
 	local OLD_PATH="${PATH}"
 	export PATH="${TERMUX_PREFIX}/bin:${PATH}"
 
+	termux_setup_cmake
 	cmake ${TERMUX_PKG_SRCDIR} \
 		-G Ninja \
 		-DCMAKE_BUILD_TYPE=Release \
