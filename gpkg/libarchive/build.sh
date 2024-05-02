f71356126662a036c9a42ea82754474c3bffd833
Ivan Max <mixython@gmail.com>
2024-05-02 17:37:51 +0300

    update pkgs (#232)
    gpkg/cmake
    gpkg/libxcb
    gpkg/mesa
    gpkg/pango
    gpkg/vulkan-headers
    gpkg/xcb-proto
    gpkg/vulkan-headers
    gpkg/vulkan-tools
    gpkg/fribidi
    gpkg/xorg-util-macros
    gpkg/xorgproto
    gpkg/libarchive
    gpkg/libclc
    gpkg/coreutils

--- gpkg/libarchive/build.sh
+++ gpkg/libarchive/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.libarchive.org/
 TERMUX_PKG_DESCRIPTION="Multi-format archive and compression library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="3.7.3"
+TERMUX_PKG_VERSION="3.7.4"
 TERMUX_PKG_SRCURL=https://github.com/libarchive/libarchive/releases/download/v$TERMUX_PKG_VERSION/libarchive-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f27a97bc22ceb996e72502df47dc19f99f9a0f09181ae909f09f3c9eb17b67e2
+TERMUX_PKG_SHA256=7875d49596286055b52439ed42f044bd8ad426aa4cc5aabd96bfe7abb971d5e8
 TERMUX_PKG_DEPENDS="libacl-glibc, libbz2-glibc, libexpat-glibc, liblz4-glibc, openssl-glibc, liblzma-glibc, zlib-glibc, zstd-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-xml2
--- gpkg/mesa/build.sh
+++ gpkg/mesa/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="An open-source implementation of the OpenGL specificatio
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="24.0.4"
+TERMUX_PKG_VERSION="24.0.6"
 TERMUX_PKG_SRCURL="https://archive.mesa3d.org/mesa-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256="90febd30a098cbcd97ff62ecc3dcf5c93d76f7fa314de944cfce81951ba745f0"
+TERMUX_PKG_SHA256="8b7a92dbe6468c18f2383700135b5fe9de836cdf0cc8fd7dbae3c7110237d604"
 TERMUX_PKG_DEPENDS="libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_SUGGESTS="mesa-dev-glibc"
 TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc"
@@ -46,5 +46,8 @@ termux_step_pre_configure() {
 		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast,panfrost,freedreno -Dfreedreno-kmds=msm,kgsl";;
 		*) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast,panfrost";;
 	esac
+	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
 	export LLVM_CONFIG=$TERMUX_PREFIX/bin/llvm-config
+	echo "${TERMUX_PKG_VERSION}.termux-glibc-${TERMUX_PKG_REVISION:=0}" > ${TERMUX_PKG_SRCDIR}/VERSION
+	rm ${TERMUX_PKG_SRCDIR}/subprojects/lua.wrap
 }
