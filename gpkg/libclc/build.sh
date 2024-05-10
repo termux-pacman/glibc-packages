2047f0c51697f17646eeb1902e21924d462886cf
Ivan Max <mixython@gmail.com>
2024-05-11 00:53:20 +0300

    update pkgs (#243)
    gpkg/bash-completion
    gpkg/cmake
    gpkg/glib
    gpkg/libclc
    gpkg/libjpeg-turbo
    gpkg/libsqlite
    gpkg/mesa
    gpkg/vulkan-tools
    gpkg/libdrm
    gpkg/postgresql
    gpkg/wget
    gpkg/vulkan-headers
    gpkg/vulkan-icd-loader

--- gpkg/libclc/build.sh
+++ gpkg/libclc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://libclc.llvm.org/
 TERMUX_PKG_DESCRIPTION="Library requirements of the OpenCL C programming language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=18.1.4
+TERMUX_PKG_VERSION=18.1.5
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/libclc-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=81e687138a35eeaee70f0fdce4a7a892b94e3c0f310e3f0efa640efd0af2c29f
+TERMUX_PKG_SHA256=3e54662d1897b1af5ec6d9cd04831393cab428d1a93814ad3812255ae2aa0d45
 TERMUX_PKG_BUILD_DEPENDS="clang-glibc, python-glibc, spirv-llvm-translator-glibc"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
