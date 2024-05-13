282ac58458ecb76aeec5ce8c6bad5cca965280ad
Ivan Max <mixython@gmail.com>
2024-05-13 15:40:05 +0300

    adding and updating packages (#245)
    add(gpkg/dconf)
    add(gpkg/jbigkit)
    add(gpkg/libcloudproviders)
    add(gpkg/libtiff)
    
    bump(gpkg/linux-api-headers): 6.8
    
    fix(gpkg/dbus): correct command launch

---
+++ gpkg/libtiff/libtiff-utils.subpackage.sh
@@ -0,0 +1,2 @@
+TERMUX_SUBPKG_INCLUDE="glibc/bin glibc/share/man"
+TERMUX_SUBPKG_DESCRIPTION="Tools for working with tiff files"
--- gpkg/linux-api-headers/build.sh
+++ gpkg/linux-api-headers/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libc/
 TERMUX_PKG_DESCRIPTION="Kernel headers sanitized for use in userspace"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=6.7
+TERMUX_PKG_VERSION=6.8
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/kernel/v${TERMUX_PKG_VERSION:0:1}.x/linux-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ef31144a2576d080d8c31698e83ec9f66bf97c677fa2aaf0d5bbb9f3345b1069
+TERMUX_PKG_SHA256=c969dea4e8bb6be991bbf7c010ba0e0a5643a3a8d8fb0a2aaa053406f1e965f3
 TERMUX_PKG_BUILD_IN_SRC=true
 
 _target_arch="x86"
