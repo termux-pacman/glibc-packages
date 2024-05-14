555de687c4c0a9452196bb3df28ff3438862eee9
Ivan Max <mixython@gmail.com>
2024-05-14 13:04:24 +0300

    update pkgs (#246)
    gpkg/assimp
    gpkg/harfbuzz
    gpkg/libnghttp2
    gpkg/libxaw
    gpkg/libxml2
    gpkg/vulkan-icd-loader
    gpkg/vulkan-tools
    gpkg/alsa-lib
    gpkg/libmpg123
    gpkg/libpam
    gpkg/libunwind
    gpkg/libva
    gpkg/mpdecimal
    gpkg/pkgconf
    gpkg/publicsuffix-list

--- gpkg/libpam/build.sh
+++ gpkg/libpam/build.sh
@@ -2,14 +2,13 @@ TERMUX_PKG_HOMEPAGE=http://linux-pam.org
 TERMUX_PKG_DESCRIPTION="PAM (Pluggable Authentication Modules) library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.5.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://github.com/linux-pam/linux-pam/releases/download/v$TERMUX_PKG_VERSION/Linux-PAM-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=7ac4b50feee004a9fa88f1dfd2d2fa738a82896763050cd773b3c54b0a818283
+TERMUX_PKG_SHA256=f8923c740159052d719dbfc2a2f81942d68dd34fcaf61c706a02c9b80feeef8e
 TERMUX_PKG_DEPENDS="libxcrypt-glibc, gcc-libs-glibc"
 TERMUX_PKG_RM_AFTER_INSTALL="glibc/bin"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --includedir=$TERMUX_PREFIX/include/security
---enable-logind
+--disable-logind
 --disable-db
 "
--- gpkg/libva/build.sh
+++ gpkg/libva/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://01.org/linuxmedia/vaapi
 TERMUX_PKG_DESCRIPTION="Video Acceleration (VA) API for Linux"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=2.20.0
-TERMUX_PKG_SRCURL=https://github.com/intel/libva/releases/download/$TERMUX_PKG_VERSION/libva-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=f72bdb4f48dfe71ad01f1cbefe069672a2c949a6abd51cf3c4d4784210badc49
+TERMUX_PKG_VERSION=2.21.0
+TERMUX_PKG_SRCURL=https://github.com/intel/libva/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f7c3fffef3f04eb146e036dad2587d852bfb70e4926d014bf437244915ef7425
 TERMUX_PKG_DEPENDS="libdrm-glibc, libglvnd-glibc, libx11-glibc, libxext-glibc, libxfixes-glibc, libwayland-glibc"
 
 termux_step_configure() {
