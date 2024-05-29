88b5229861c251e06d57d3f046b9362c1fa8ceb1
Maxython <mixython@gmail.com>
2024-05-30 00:32:02 +0300

    gpkg/glibc: global update
     - added the glibc32 subpackage
     - updated fakesyscall handling scheme
     - added all disabled syscalls to fakesyscall list
     - added new syscalls to fakesyscall list
     - disabled removal of old locales in `locale-gen`
     - implemented syslog function, which is configured to work with the android log system
     - implemented analog variables `LD_*` to customize glibc environment without affecting bionic environment
     - improved source code for flexible glibc compilation

--- gpkg/glibc/build.sh
+++ gpkg/glibc/build.sh
@@ -3,74 +3,163 @@ TERMUX_PKG_DESCRIPTION="GNU C Library"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=2.39
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libc/glibc-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=f77bd47cf8170c57365ae7bf86696c118adb3b120d3259c64c502d3dc1e2d926
 TERMUX_PKG_DEPENDS="linux-api-headers-glibc"
 TERMUX_PKG_RECOMMENDS="glibc-runner"
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_CONFFILES="glibc/etc/gai.conf, glibc/etc/locale.gen"
+TERMUX_PKG_SEPARATE_SUB_DEPENDS=true
+
+# Variables for glibc32 compilation
+TERMUX_PKG_BUILD32=$(test "$TERMUX_ARCH" = "aarch64" || test "$TERMUX_ARCH" = "arm" && \
+		     test "$TERMUX_ON_DEVICE_BUILD" = "false" && echo "true" || echo "false")
+TERMUX_PKG_BUILDDIR32="${TERMUX_TOPDIR}/${TERMUX_PKG_NAME}/build32"
+#if [ "$TERMUX_PKG_BUILD32" = "true" ]; then
+#	TERMUX_PKG_BUILD_DEPENDS="glibc32"
+#fi
+
+termux_setup_build32() {
+	case $TERMUX_ARCH in
+		"aarch64")
+			TERMUX_ARCH="arm"
+			TERMUX_HOST_PLATFORM="arm-linux-gnueabihf";;
+		"x86_64")
+			TERMUX_ARCH="i686"
+			TERMUX_HOST_PLATFORM="i686-linux-gnu";;
+	esac
+	termux_step_setup_toolchain
+	cd ${TERMUX_PKG_BUILDDIR32}
+}
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_PACKAGE_LIBRARY" != "glibc" ]; then
 		termux_error_exit "Compilation is only possible based on glibc"
 	fi
 
-	for i in shmem-android.h shmat.c  shmctl.c  shmdt.c  shmget.c mprotect.c syscall.c fake-syscall.h; do
-		install -Dm644 "${TERMUX_PKG_BUILDER_DIR}/${i}" "${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}"
-	done
-
+	# disabling clone3 function
 	rm ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/*/clone3.S
+	# disabling editing of `ldd` script for x86_64 arch
 	rm ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/x86_64/configure*
 
-	if [ "$TERMUX_ARCH" = "i686" ]; then
-		mv ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/i386/syscall.S \
-			${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/i386/syscallS.S
-	else
-		mv ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${TERMUX_ARCH}/syscall.S \
-			${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${TERMUX_ARCH}/syscallS.S
-	fi
+	# installing special scripts for correct operation of system calls
+	for i in ${TERMUX_PKG_BUILDER_DIR}/{shm{at,ctl,dt,get}.c,mprotect.c,syscall.c,fakesyscall*.h,fake_epoll_pwait2.c}; do
+		cp ${i} ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/
+	done
 
-	for i in android_passwd_group.h android_passwd_group.c android_system_user_ids.h; do
-		cp ${TERMUX_PKG_BUILDER_DIR}/${i} ${TERMUX_PKG_SRCDIR}/nss/
+	# installing and configuring scripts for parsing users/groups according to the android standard
+	for i in ${TERMUX_PKG_BUILDER_DIR}/{android_passwd_group.*,android_system_user_ids.h}; do
+		cp ${i} ${TERMUX_PKG_SRCDIR}/nss/
 	done
 	bash ${TERMUX_PKG_BUILDER_DIR}/gen-android-ids.sh ${TERMUX_BASE_DIR} \
 		${TERMUX_PKG_SRCDIR}/nss/android_ids.h \
 		${TERMUX_PKG_BUILDER_DIR}/android_system_user_ids.h
 
-	# `disabled-syscalls` - a file that contains a list of system calls that should be disabled
+	# installing a syslog script that can work with the android log system
+	cp ${TERMUX_PKG_BUILDER_DIR}/syslog.c ${TERMUX_PKG_SRCDIR}/misc/
+
+	# installing shmem-android scripts for system V shared memory emulation
+	cp ${TERMUX_PKG_BUILDER_DIR}/shmem-android.* ${TERMUX_PKG_SRCDIR}/sysvipc/
+
+	# `fakesyscall.json` - json file that stores a list of unsupported syscalls for Termux in keys,
+	# the name of which indicates the fakesyscall function and how it will be launched
+	# == Syntax ==
+	# {
+	#   "fakesyscall_1()": [
+	#     "syscall_1",
+	#     "syscall_2"
+	#   ],
+	#   "fakesyscall_2(a0, a1, a2, a3, a4, a5)": [
+	#     "syscall_3",
+	#     "syscall_4"
+	#   ]
+	# }
+	# == Rules ===
+	# - The name syscall and the name of the function fakesyscall must not be repeated
+	# - Specify the syscall name without the leading `__NR_` prefix
+	# ============
 	for i in aarch64 arm i386 x86_64/64; do
+		mv ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i///*/}/syscall.S \
+			${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i///*/}/syscallS.S
+		local header_disabled_syscall="${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/disabled-syscall.h"
 		{
-			for j in $(awk '{printf "__NR_" $1 " "}' ${TERMUX_PKG_BUILDER_DIR}/disabled-syscalls); do
-				grep "#define ${j} " ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/arch-syscall.h || true
-				sed -i "/#define ${j} /d" ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/arch-syscall.h
+			for j in $(jq -r '.[] | .[]' ${TERMUX_PKG_BUILDER_DIR}/fakesyscall.json); do
+				grep "#define __NR_${j} " ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/arch-syscall.h || true
+				sed -i "/#define __NR_${j} /d" ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/arch-syscall.h
 			done
-		} >> ${TERMUX_PKG_SRCDIR}/sysdeps/unix/sysv/linux/${i}/disabled-syscall.h
+		} >> $header_disabled_syscall
+		{
+			echo -e "\n#define DISABLED_SYSCALL_WITH_FAKESYSCALL \\"
+			local IFS=$'\n'
+			for j in $(jq -r '. | keys | .[]' ${TERMUX_PKG_BUILDER_DIR}/fakesyscall.json); do
+				local need_return=false
+				for z in $(jq -r '."'${j}'" | .[]' ${TERMUX_PKG_BUILDER_DIR}/fakesyscall.json); do
+					if grep -q "^#define __NR_${z} " $header_disabled_syscall; then
+						echo -e "\tcase __NR_${z}: \\"
+						need_return=true
+					elif [[ ${z} =~ ^[0-9]+$ ]]; then
+						echo -e "\tcase ${z}: \\"
+						need_return=true
+					fi
+				done
+				[ "${need_return}" = "true" ] && echo -e "\t\treturn ${j}; \\"
+			done
+			unset IFS
+		} >> $header_disabled_syscall
+		sed -i '$ s| \\||' $header_disabled_syscall
 	done
+
+	# replacing some hard paths that may not exist in some device
+	for i in /dev/stderr:/proc/self/fd/2 \
+		 /dev/stdin:/proc/self/fd/0 \
+		 /dev/stdout:/proc/self/fd/1; do
+		for j in $(grep -s -r -l ${i%%:*} ${TERMUX_PKG_SRCDIR}); do
+			sed -i "s|${i%%:*}|${i//*:}|g" ${j}
+		done
+	done
+
+	# adding revision to glibc version
+	sed -i "s/${TERMUX_PKG_VERSION}/${TERMUX_PKG_FULLVERSION_FOR_PACMAN}/" ${TERMUX_PKG_SRCDIR}/version.h
+
+	# specifying the current release (use only when developing glibc)
+	sed -i "s/stable/dev.$(git -C ${TERMUX_PKG_BUILDER_DIR} rev-parse --short HEAD).$(date +%Y%m%d%H%M%S)/" ${TERMUX_PKG_SRCDIR}/version.h
+
+	if [ "$TERMUX_PKG_BUILD32" = "true" ]; then
+		rm -fr ${TERMUX_PKG_BUILDDIR32}
+		mkdir -p ${TERMUX_PKG_BUILDDIR32}
+	fi
 }
 
-termux_step_configure() {
-	echo "slibdir=${TERMUX_PREFIX}/lib" > configparms
-	echo "rtlddir=${TERMUX_PREFIX}/lib" >> configparms
+termux_glibc_configure() {
+	local libdir="${1}"
+
+	echo "slibdir=${TERMUX_PREFIX}/${libdir}" > configparms
+	echo "rtlddir=${TERMUX_PREFIX}/${libdir}" >> configparms
 	echo "sbindir=${TERMUX_PREFIX}/bin" >> configparms
 	echo "rootsbindir=${TERMUX_PREFIX}/bin" >> configparms
 
 	local _configure_flags=()
 	case $TERMUX_ARCH in
 		"aarch64") _configure_flags+=(--enable-memory-tagging --enable-fortify-source);;
-		"arm") _configure_flags+=(--enable-fortify-source);;
+		"arm"|"i686") _configure_flags+=(--enable-fortify-source);;
 		"x86_64") _configure_flags+=(--enable-cet);;
-		"i686") _configure_flags+=(--enable-fortify-source);;
 	esac
+
+	local _pkgversion="GNU libc for Android"
+	if [ -n "${TERMUX_APP_PACKAGE-}" ]; then
+		_pkgversion+="/${TERMUX_APP_PACKAGE}"
+	fi
 
 	${TERMUX_PKG_SRCDIR}/configure \
 		--prefix=$TERMUX_PREFIX \
-		--libdir=${TERMUX_PREFIX}/lib \
-		--libexecdir=${TERMUX_PREFIX}/lib \
+		--libdir=${TERMUX_PREFIX}/${libdir} \
+		--libexecdir=${TERMUX_PREFIX}/${libdir} \
 		--host=$TERMUX_HOST_PLATFORM \
 		--build=$TERMUX_HOST_PLATFORM \
 		--target=$TERMUX_HOST_PLATFORM \
 		--with-bugurl=https://github.com/termux-pacman/glibc-packages/issues \
+		--with-pkgversion="${_pkgversion}" \
 		--enable-bind-now \
 		--disable-multi-arch \
 		--enable-stack-protector=strong \
@@ -82,11 +171,37 @@ termux_step_configure() {
 		"${_configure_flags[@]}"
 }
 
+termux_step_configure() {
+	termux_glibc_configure "lib"
+
+	if [ "$TERMUX_PKG_BUILD32" = "true" ]; then
+		(
+			termux_setup_build32
+			termux_glibc_configure "lib32"
+			echo 'build-programs=no' >> configparms
+		)
+	fi
+}
+
 termux_step_make() {
 	make -O
 	make info
 
-	elf/ld.so --library-path "$PWD" locale/localedef -c -f ${TERMUX_PKG_SRCDIR}/localedata/charmaps/UTF-8 -i ${TERMUX_PKG_SRCDIR}/localedata/locales/C ./C.UTF-8/
+	if [ "$TERMUX_PKG_BUILD32" = "true" ]; then
+		(
+			termux_setup_build32
+			make -O
+		)
+	fi
+}
+
+termux_glibc_make_syscall_without_fsc() {
+	local libname="libsyscall_without_fsc.so"
+	local libdir="$1"
+	echo "Compiling '${libname}'..."
+	$CC ${TERMUX_PKG_BUILDER_DIR}/syscall.c -o ${TERMUX_PREFIX}/${libdir}/${libname} \
+		-shared -DWITHOUT_FAKESYSCALL
+	echo "DONE"
 }
 
 termux_step_make_install() {
@@ -113,12 +228,32 @@ termux_step_make_install() {
 		${TERMUX_PKG_SRCDIR}/localedata/SUPPORTED > ${TERMUX_PREFIX}/share/i18n/SUPPORTED
 
 	install -dm755 ${TERMUX_PREFIX}/lib/locale
-	cp -r ./C.UTF-8 -t ${TERMUX_PREFIX}/lib/locale
+	make -C ${TERMUX_PKG_SRCDIR}/localedata objdir=${TERMUX_PKG_BUILDDIR} \
+		SUPPORTED-LOCALES="C.UTF-8/UTF-8 en_US.UTF-8/UTF-8" install-locale-files
 	sed -i '/#C\.UTF-8 /d' ${TERMUX_PREFIX}/etc/locale.gen
 
 	install -Dm644 ${TERMUX_PKG_BUILDER_DIR}/sdt.h ${TERMUX_PREFIX}/include/sys/sdt.h
 	install -Dm644 ${TERMUX_PKG_BUILDER_DIR}/sdt-config.h ${TERMUX_PREFIX}/include/sys/sdt-config.h
 
-	ln -sf $PATH_DYNAMIC_LINKER $TERMUX_PREFIX/bin/ld.so
-	ln -sf $PATH_DYNAMIC_LINKER $TERMUX_PREFIX/lib/ld.so
+	ln -sfr $PATH_DYNAMIC_LINKER ${TERMUX_PREFIX}/bin/ld.so
+	ln -sfr $PATH_DYNAMIC_LINKER ${TERMUX_PREFIX}/lib/ld.so
+
+	termux_glibc_make_syscall_without_fsc "lib"
+
+	if [ "$TERMUX_PKG_BUILD32" = "true" ]; then
+		(
+			termux_setup_build32
+
+			make DESTDIR=${TERMUX_PKG_BUILDDIR32} install
+
+			cp -r ${TERMUX_PKG_BUILDDIR32}/${TERMUX_PREFIX}/lib32 $TERMUX_PREFIX
+
+			ln -sfr ${TERMUX_PREFIX}/lib/locale ${TERMUX_PREFIX}/lib32/locale
+
+			ln -sfr ${TERMUX_PREFIX}/lib32/${DYNAMIC_LINKER} $PATH_DYNAMIC_LINKER
+			ln -sfr ${TERMUX_PREFIX}/lib32/${DYNAMIC_LINKER} ${TERMUX_PREFIX}/lib32/ld.so
+
+			termux_glibc_make_syscall_without_fsc "lib32"
+		)
+	fi
 }
--- gpkg/glibc/shmctl.c
+++ gpkg/glibc/shmctl.c
@@ -1,18 +1,10 @@
-#include <stdarg.h>
-#include <ipc_priv.h>
-#include <sysdep.h>
-#include <shlib-compat.h>
-#include <errno.h>
 #include <shmem-android.h>
+#include <stdarg.h>
+#include <shlib-compat.h>
 #include <asm-generic/ipcbuf.h>
 #include <asm-generic/shmbuf.h>
 
-#ifndef shmid_ds
-# define shmid_ds shmid64_ds
-#endif
-
-int shmctl(int shmid, int cmd, struct shmid_ds *buf)
-{
+int __shmctl64(int shmid, int cmd, struct __shmid64_ds *buf) {
 	ashv_check_pid();
 
 	if (cmd == IPC_RMID) {
@@ -55,13 +47,13 @@ int shmctl(int shmid, int cmd, struct shmid_ds *buf)
 		memset(buf, 0, sizeof(struct shmid_ds));
 		buf->shm_segsz = shmem[idx].size;
 		buf->shm_nattch = 1;
-		buf->shm_perm.key = shmem[idx].key;
+		buf->shm_perm.__key = shmem[idx].key;
 		buf->shm_perm.uid = geteuid();
 		buf->shm_perm.gid = getegid();
 		buf->shm_perm.cuid = geteuid();
 		buf->shm_perm.cgid = getegid();
 		buf->shm_perm.mode = 0666;
-		buf->shm_perm.seq = 1;
+		buf->shm_perm.__seq = 1;
 
 		pthread_mutex_unlock (&mutex);
 		return 0;
@@ -72,4 +64,66 @@ int shmctl(int shmid, int cmd, struct shmid_ds *buf)
 	return -1;
 }
 
-weak_alias (shmctl, __shmctl64)
+#if __TIMESIZE != 64
+libc_hidden_def(__shmctl64)
+
+static void shmid_to_shmid64(struct __shmid64_ds *shm64, const struct shmid_ds *shm) {
+	shm64->shm_perm   = shm->shm_perm;
+	shm64->shm_segsz  = shm->shm_segsz;
+	shm64->shm_atime  = shm->shm_atime | ((__time64_t) shm->__shm_atime_high << 32);
+	shm64->shm_dtime  = shm->shm_dtime | ((__time64_t) shm->__shm_dtime_high << 32);
+	shm64->shm_ctime  = shm->shm_ctime | ((__time64_t) shm->__shm_ctime_high << 32);
+	shm64->shm_cpid   = shm->shm_cpid;
+	shm64->shm_lpid   = shm->shm_lpid;
+	shm64->shm_nattch = shm->shm_nattch;
+}
+
+static void shmid64_to_shmid(struct shmid_ds *shm, const struct __shmid64_ds *shm64) {
+	shm->shm_perm         = shm64->shm_perm;
+	shm->shm_segsz        = shm64->shm_segsz;
+	shm->shm_atime        = shm64->shm_atime;
+	shm->__shm_atime_high = 0;
+	shm->shm_dtime        = shm64->shm_dtime;
+	shm->__shm_dtime_high = 0;
+	shm->shm_ctime        = shm64->shm_ctime;
+	shm->__shm_ctime_high = 0;
+	shm->shm_cpid         = shm64->shm_cpid;
+	shm->shm_lpid         = shm64->shm_lpid;
+	shm->shm_nattch       = shm64->shm_nattch;
+}
+
+int __shmctl(int shmid, int cmd, struct shmid_ds *buf) {
+	struct __shmid64_ds shmid64, *buf64 = NULL;
+	if (buf != NULL) {
+		if (cmd == IPC_INFO || cmd == SHM_INFO)
+			buf64 = (struct __shmid64_ds *) buf;
+		else {
+			shmid_to_shmid64 (&shmid64, buf);
+			buf64 = &shmid64;
+		}
+	}
+
+	int ret = __shmctl64 (shmid, cmd, buf64);
+	if (ret < 0)
+		return ret;
+
+	switch (cmd) {
+		case IPC_STAT:
+		case SHM_STAT:
+		case SHM_STAT_ANY:
+			shmid64_to_shmid (buf, buf64);
+	}
+
+	return ret;
+}
+#endif
+
+#ifndef DEFAULT_VERSION
+# ifndef __ASSUME_SYSVIPC_BROKEN_MODE_T
+#  define DEFAULT_VERSION GLIBC_2_2
+# else
+#  define DEFAULT_VERSION GLIBC_2_31
+# endif
+#endif
+
+versioned_symbol(libc, __shmctl, shmctl, DEFAULT_VERSION);
