4796bfee82b6ec47450e7db82ab12ec9d6dd7c09
Ivan Max <mixython@gmail.com>
2024-05-30 20:04:27 +0300

    update pkgs (#252)
    gpkg/libclc
    gpkg/openssl
    gpkg/libcurl
    gpkg/mesa
    gpkg/openldap
    gpkg/util-linux
    gpkg/e2fsprogs
    gpkg/libdav1d
    gpkg/libpixman
    gpkg/libsodium
    gpkg/libsqlite
    gpkg/libaom

--- get-build-package.sh
+++ get-build-package.sh
@@ -7,6 +7,7 @@ BRANCH="master"
 git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
 
 for i in build-package.sh clean.sh packages x11-packages scripts; do
+	rm -fr ./${i}
 	cp -r ./termux-packages/${i} ./
 done
 
