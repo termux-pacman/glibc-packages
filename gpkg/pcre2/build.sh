1002e6f9c30e9c228c9b4056b164d39583bf945a
Ivan Max <mixython@gmail.com>
2024-06-08 19:15:50 +0300

    update pkgs (#254)
    gpkg/box64
    gpkg/box86
    gpkg/git
    gpkg/libdrm
    gpkg/libedit
    gpkg/liblzma
    gpkg/mariadb
    gpkg/ncurses
    gpkg/cmake
    gpkg/fakeroot
    gpkg/fribidi
    gpkg/libopus
    gpkg/openssl
    gpkg/pcre2
    gpkg/vulkan-headers
    gpkg/vulkan-icd-loader
    gpkg/vulkan-tools

--- gpkg/pcre2/build.sh
+++ gpkg/pcre2/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.pcre.org
 TERMUX_PKG_DESCRIPTION="Perl 5 compatible regular expression library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="10.43"
+TERMUX_PKG_VERSION="10.44"
 TERMUX_PKG_SRCURL=https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${TERMUX_PKG_VERSION}/pcre2-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=e2a53984ff0b07dfdb5ae4486bbb9b21cca8e7df2434096cc9bf1b728c350bcb
+TERMUX_PKG_SHA256=d34f02e113cf7193a1ebf2770d3ac527088d485d4e047ed10e5d217c6ef5de96
 TERMUX_PKG_DEPENDS="readline-glibc, zlib-glibc, bash-glibc, libbz2-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-pcre2-16
