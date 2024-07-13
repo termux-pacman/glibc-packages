5f9a39ac969f70293a409bc7cebe4d4f2d29e357
Ivan Max <mixython@gmail.com>
2024-07-13 11:36:55 +0300

    update pkgs (#264)
    gpkg/apache2
    gpkg/assimp
    gpkg/ca-certificates
    gpkg/cmake
    gpkg/glib
    gpkg/glslang
    gpkg/less
    gpkg/libxml2
    gpkg/p11-kit
    gpkg/python-pip
    gpkg/spirv-llvm-translator
    gpkg/spirv-tools
    gpkg/vulkan-headers
    gpkg/vulkan-icd-loader
    gpkg/vulkan-tools
    gpkg/xkeyboard-config
    gpkg/util-linux

--- gpkg/less/build.sh
+++ gpkg/less/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=643
+TERMUX_PKG_VERSION=661
 TERMUX_PKG_SRCURL=https://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2911b5432c836fa084c8a2e68f6cd6312372c026a58faaa98862731c8b6052e8
+TERMUX_PKG_SHA256=2b5f0167216e3ef0ffcb0c31c374e287eb035e4e223d5dae315c2783b6e738ed
 TERMUX_PKG_DEPENDS="ncurses-glibc, pcre2-glibc"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-regex=pcre2"
