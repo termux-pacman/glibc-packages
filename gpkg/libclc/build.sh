358d8a6c49e61c097603d9c8fcd63bb0ba997d4a
Ivan Max <mixython@gmail.com>
2024-07-15 14:41:15 +0300

    update pkgs (#268)
    gpkg/libclc
    gpkg/mesa
    gpkg/vulkan-icd-loader
    gpkg/vulkan-tools

--- gpkg/libclc/build.sh
+++ gpkg/libclc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://libclc.llvm.org/
 TERMUX_PKG_DESCRIPTION="Library requirements of the OpenCL C programming language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=18.1.7
+TERMUX_PKG_VERSION=18.1.8
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/libclc-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=bf99fdabd64ebfc688775754edf4c6bd3ffc361906b710ee49107e03fd3db396
+TERMUX_PKG_SHA256=905bd59e9f810d6bd0ae6874725a8f8a3c91cb416199c03f2b98b57437cfb32e
 TERMUX_PKG_BUILD_DEPENDS="clang-glibc, python-glibc, spirv-llvm-translator-glibc"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
