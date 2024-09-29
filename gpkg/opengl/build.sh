690ca6f00e7dbeec152ede5cd192c4daaaed98ac
Ivan Max <mixython@gmail.com>
2024-09-30 00:19:22 +0300

    update pkgs (#287)
    gpkg/glib
    gpkg/libarchive
    gpkg/cmake
    gpkg/fribidi
    gpkg/git
    gpkg/harfbuzz
    gpkg/libuv
    
    recompilation with new python:
    - gpkg/asciidoc
    - gpkg/libxcb
    - gpkg/libxslt
    - gpkg/opengl
    - gpkg/subversion
    - gpkg/xcb-proto
    - gpkg/gobject-introspection
    - gpkg/libxml2
    - gpkg/util-linux

--- gpkg/opengl/build.sh
+++ gpkg/opengl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A metapackage for OpenGL implementation"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_DEPENDS="libglvnd-glibc, mesa-glibc"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="mesa-glibc"
