21ce4cccbf011d015c205727d02b70525e1e68c0
Ivan Max <mixython@gmail.com>
2024-10-07 09:10:31 +0300

    bump(gpkg/libclc): to 19.1.1 (#295)

--- gpkg/libclc/build.sh
+++ gpkg/libclc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://libclc.llvm.org/
 TERMUX_PKG_DESCRIPTION="Library requirements of the OpenCL C programming language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=18.1.8
+TERMUX_PKG_VERSION=19.1.1
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/libclc-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=905bd59e9f810d6bd0ae6874725a8f8a3c91cb416199c03f2b98b57437cfb32e
+TERMUX_PKG_SHA256=2872099fab914f02dfaa3fd42767b93fbcc6027289433c5263d693f5fd73e189
 TERMUX_PKG_BUILD_DEPENDS="clang-glibc, python-glibc, spirv-llvm-translator-glibc"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
