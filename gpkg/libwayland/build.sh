5493d7dedfd531b85b048903d43d0ac59f2bbf91
Maxython <mixython@gmail.com>
2024-11-06 14:01:47 +0300

    update pkgs
    gpkg/glib
    gpkg/harfbuzz
    gpkg/libwayland
    gpkg/mesa
    gpkg/ncurses
    gpkg/subversion
    gpkg/vulkan-headers
    gpkg/vulkan-icd-loader
    gpkg/vulkan-tools
    gpkg/cmake
    gpkg/libcurl
    gpkg/libexpat
    gpkg/libxcursor
    gpkg/xtrans
    gpkg/alsa-lib

--- gpkg/libwayland/build.sh
+++ gpkg/libwayland/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocol library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.22.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.23.1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=1540af1ea698a471c2d8e9d288332c7e0fd360c8f1d12936ebb7e7cbc2425842
+TERMUX_PKG_SHA256=864fb2a8399e2d0ec39d56e9d9b753c093775beadc6022ce81f441929a81e5ed
 TERMUX_PKG_DEPENDS="libffi-glibc, libexpat-glibc, libxml2-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Ddocumentation=false"
+TERMUX_PKG_MESON_NATIVE=true
