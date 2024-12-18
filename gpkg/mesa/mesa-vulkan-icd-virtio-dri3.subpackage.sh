db7892ca420346ce1180e199670a66cc391c25a9
Adya Roby <adyaro37@gmail.com>
2024-12-18 16:44:43 +0700

    Update mesa-vulkan-icd-virtio-dri3.subpackage.sh

--- gpkg/mesa/mesa-vulkan-icd-virtio-dri3.subpackage.sh
+++ gpkg/mesa/mesa-vulkan-icd-virtio-dri3.subpackage.sh
@@ -1,8 +1,8 @@
 TERMUX_SUBPKG_DESCRIPTION="Mesa's Virtio Vulkan ICD"
 TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
-TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, vulkan-loader-generic, zlib, zstd"
+TERMUX_SUBPKG_DEPENDS="vulkan-icd-loader-glibc, gcc-libs-glibc, zlib-glibc, zstd-glibc, libxcb-glibc, libdrm-glibc, libx11-glibc, libxshmfence-glibc, libexpat-glibc, libwayland-glibc"
 TERMUX_SUBPKG_EXCLUDED_ARCHES="arm, i686, x86_64"
 TERMUX_SUBPKG_INCLUDE="
-lib/libvulkan_virtio.so
-share/vulkan/icd.d/virtio_icd.*.json
+glibc/lib/libvulkan_virtio.so
+glibc/share/vulkan/icd.d/virtio_icd.*.json
 "
