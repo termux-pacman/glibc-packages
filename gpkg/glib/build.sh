7d025d57f59ba750a19e2e0aea065326e25b55b8
Maxython <mixython@gmail.com>
2025-03-10 13:56:05 +0300

    global packages update
    box64
    cmake
    coreutils
    fakeroot
    file
    fontconfig
    freeglut
    gifsicle
    git
    glib
    glslang
    harfbuzz
    less
    libsm
    libx11
    mangohud
    mariadb
    nano
    openldap
    pango
    strace
    subversion
    vkmark
    vulkan-headers
    vulkan-icd-loader
    vulkan-tools
    vulkan-volk
    wget
    xkeyboard-config
    xtrans
    spirv-tools

--- gpkg/glib/build.sh
+++ gpkg/glib/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="2.82.2"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2.84.0"
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION%.*}/glib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ab45f5a323048b1659ee0fbda5cecd94b099ab3e4b9abf26ae06aeb3e781fd63
+TERMUX_PKG_SHA256=f8823600cb85425e2815cfad82ea20fdaa538482ab74e7293d58b3f64a5aff6a
 TERMUX_PKG_DEPENDS="libffi-glibc, pcre2-glibc, util-linux-glibc, zlib-glibc, openssl-glibc, libunwind-glibc, libmount-glibc"
 TERMUX_PKG_BUILD_DEPENDS="gobject-introspection-glibc"
 TERMUX_PKG_PYTHON_COMMON_DEPS="pygments, itstool, packaging, gi-docgen"
 TERMUX_PKG_ACCEPT_PKG_IN_DEP=true
-# -D sysprof=enabled
+TERMUX_PKG_MESON_NATIVE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --default-library both
 -D runtime_dir=$TERMUX_PREFIX/var/run
@@ -22,8 +21,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	CFLAGS+=" -g3 -ffat-lto-objects"
-	CXXFLAGS+=" -g3 -ffat-lto-objects"
+	CFLAGS+=" -ffat-lto-objects"
+	CXXFLAGS+=" -ffat-lto-objects"
 }
 
 termux_step_create_debscripts() {
--- gpkg/vkmark/build.sh
+++ gpkg/vkmark/build.sh
@@ -2,30 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vkmark/vkmark
 TERMUX_PKG_DESCRIPTION="vkmark is an extensible Vulkan benchmarking suite with targeted, configurable scenes."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-_COMMIT=ab6e6f34077722d5ae33f6bd40b18ef9c0e99a15
-TERMUX_PKG_VERSION=2023.04.12
-TERMUX_PKG_SRCURL=git+https://github.com/vkmark/vkmark.git
-TERMUX_PKG_SHA256=2fde566a542fddc5c74a62c40dcb2d62e1151c7fbdc395adb1dc21857defa09d
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION=2025.01
+TERMUX_PKG_SRCURL=https://github.com/vkmark/vkmark/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1ae362844344d0f9878b7a3f13005f77eae705108892a4e8abf237d452d37edc
 TERMUX_PKG_DEPENDS="vulkan-icd-loader-glibc, gcc-libs-glibc, assimp-glibc, xcb-util-wm-glibc, libwayland-glibc"
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers-glibc, xorgproto-glibc, libwayland-protocols-glibc, glm-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dkms=false"
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-
-	local s=($(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum))
-	if [[ ${s[0]} != $TERMUX_PKG_SHA256 ]]; then
-		echo "Expected: $TERMUX_PKG_SHA256"
-		echo "Got: ${s[0]}"
-		termux_error_exit "Checksum mismatch for source files."
-	fi
-}
