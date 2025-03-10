179748766e6735b39cbb17a03083c0987cde0343
Maxython <mixython@gmail.com>
2025-03-10 13:56:05 +0300

    global packages update
    box64
    cmake
    coreutils
    fakeroot
    file
    fontconfig
    freeglut
    gifsicle
    git
    glib
    glslang
    harfbuzz
    less
    libsm
    libx11
    mangohud
    mariadb
    nano
    openldap
    pango
    strace
    subversion
    vkmark
    vulkan-headers
    vulkan-icd-loader
    vulkan-tools
    vulkan-volk
    wget
    xkeyboard-config
    xtrans
    spirv-tools

--- gpkg/fakeroot/build.sh
+++ gpkg/fakeroot/build.sh
@@ -2,16 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://tracker.debian.org/pkg/fakeroot
 TERMUX_PKG_DESCRIPTION="Tool for simulating superuser privileges"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.36
+TERMUX_PKG_VERSION=1.37
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=7fe3cf3daf95ee93b47e568e85f4d341a1f9ae91766b4f9a9cdc29737dea4988
+TERMUX_PKG_SHA256=9831cc912bc1da6dadac15699c5a07a82c00d6f0dd5c15ec02e20908dd527d3a
 TERMUX_PKG_DEPENDS="sed-glibc, util-linux-glibc, bash-glibc, libcap-glibc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---libdir=$TERMUX_PREFIX/lib/libfakeroot
 --disable-static
 --with-ipc=tcp
 "
 
+termux_step_pre_configure() {
+	autoreconf -fi
+}
+
 termux_step_post_make_install() {
-	ln -sf libfakeroot/libfakeroot.so $TERMUX_PREFIX/lib
+	ln -sfr "${TERMUX_PREFIX}/lib/libfakeroot-0.so" "${TERMUX_PREFIX}/lib/libfakeroot.so"
 }
