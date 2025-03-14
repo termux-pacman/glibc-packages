348e41015d15d863024238a7905ea1912ffe0fc2
Maxython <mixython@gmail.com>
2025-03-14 18:35:42 +0300

    test

--- get-build-package.sh
+++ get-build-package.sh
@@ -2,9 +2,9 @@
 
 # Script that installs build-package.sh to compile glibc packages
 
-BRANCH="fix-python-cache"
+BRANCH="exclude-pycache-during-extract-into-massagedir"
 
-git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
+git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/robertkirkman/termux-packages.git
 
 for i in build-package.sh clean.sh packages x11-packages root-packages scripts ndk-patches; do
 	rm -fr ./${i}
