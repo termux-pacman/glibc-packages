cd7c6e9931e612a16b98fb4f39cf8e090ba3e8bd
Max Ivan <mixython@gmail.com>
2026-02-09 03:23:46 +0300

    update pkgs (#365)
    gpkg/linux-api-headers: bump to 6.18.7
    gpkg/glibc: bump to 2.42

--- gpkg/glibc/android_passwd_group.c
+++ gpkg/glibc/android_passwd_group.c
@@ -4,6 +4,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "android_ids.h"
--- gpkg/linux-api-headers/build.sh
+++ gpkg/linux-api-headers/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libc/
 TERMUX_PKG_DESCRIPTION="Kernel headers sanitized for use in userspace"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=6.14
+TERMUX_PKG_VERSION=6.18.7
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/kernel/v${TERMUX_PKG_VERSION:0:1}.x/linux-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a294b683e7b161bb0517bb32ec7ed1d2ea7603dfbabad135170ed12d00c47670
+TERMUX_PKG_SHA256=b726a4d15cf9ae06219b56d87820776e34d89fbc137e55fb54a9b9c3015b8f1e
 TERMUX_PKG_BUILD_MULTILIB=true
 
 termux_step_make() {
