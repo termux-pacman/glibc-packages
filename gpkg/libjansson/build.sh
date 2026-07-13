78278f30a5ed1bf4ffdfd1aa00a01e64240381ec
Maxython <mixython@gmail.com>
2026-07-13 17:45:16 +0300

    update pkgs
    bump(gpkg/cmocka): 2.0.2
    bump(gpkg/json-c): 0.19
    bump(gpkg/jsoncpp): 1.9.8
    bump(gpkg/libdav1d): 1.5.3
    bump(gpkg/libffi): 3.5.2
    bump(gpkg/libjansson): 2.15.1
    bump(gpkg/libjpeg-turbo): 3.2.0
    bump(gpkg/libmpg123): 1.33.5
    bump(gpkg/libnghttp2): 1.69.0
    bump(gpkg/libogg): 1.3.6
    bump(gpkg/libopus): 1.6.1
    bump(gpkg/libpixman): 0.46.4
    bump(gpkg/libseccomp): 2.6.0
    bump(gpkg/libsodium): 1.0.22
    bump(gpkg/libtasn1): 4.21.0
    bump(gpkg/rhash): 1.4.6
    bump(gpkg/utf8proc): 2.11.3
    bump(gpkg/which): 2.25
    bump(gpkg/zlib): 1.3.2
    
    fix(gpkg/glibc): fix undefined symbol `__vsyslog_chk`

--- gpkg/libjansson/build.sh
+++ gpkg/libjansson/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.digip.org/jansson/
 TERMUX_PKG_DESCRIPTION="C library for encoding, decoding and manipulating JSON data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_VERSION=2.15.1
 TERMUX_PKG_SRCURL=https://github.com/akheron/jansson/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c739578bf6b764aa0752db9a2fdadcfe921c78f1228c7ec0bb47fa804c55d17b
+TERMUX_PKG_SHA256=dbf95cb0af903f4fb8b61507d96b45b67db7d1479688ede352e1d571394d06f7
 TERMUX_PKG_DEPENDS="glibc"
 TERMUX_PKG_BUILD_IN_SRC=true
 
--- gpkg/zlib/build.sh
+++ gpkg/zlib/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.zlib.net/
 TERMUX_PKG_DESCRIPTION="Compression library implementing the deflate compression method found in gzip and PKZIP"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_SRCURL=https://www.zlib.net/zlib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
+TERMUX_PKG_SHA256=d7a0654783a4da529d1bb793b7ad9c3318020af77667bcae35f95d0e42a792f3
 TERMUX_PKG_DEPENDS="glibc"
 
 termux_step_configure() {
